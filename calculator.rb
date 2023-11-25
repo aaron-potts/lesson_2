c791a1148176e542be96264e358f95e2d9a4b4fc
Aaron Potts <AaronFPotts@gmail.com>
2023-11-24 20:06:57 -0600

    Start mortgage calculator

--- calculator.rb
+++ calculator.rb
@@ -5,27 +5,90 @@
   4) output the result
 =end
 
-Kernel.puts("Welcome to the calculator!")
-
-Kernel.puts("What is the first number?")
-number1 = Kernel.gets().chomp().to_i()
-
-
-Kernel.puts("What is the second number?")
-number2 = Kernel.gets().chomp().to_i()
-
-Kernel.puts("What operation would you like to perform? 1) Add 2) Subtract 3) Divide 4) Multiply")
-operator = Kernel.gets().chomp().to_i()
-
-case operator
-when 1 
-    result = number1 + number2
-when 2
-    result = number1 - number2
-when 3
-    result = number1.to_f() / number2.to_f()
-when 4
-    result = number1 * number2
+def prompt(message)
+  Kernel.puts("=> #{message}")
 end
 
-Kernel.puts("The result is #{result}")
+def valid_number?(num)
+  num.to_i != Integer
+end
+
+prompt("Welcome to the calculator! Your name, please: ")
+name = ''
+loop do
+  name = Kernel.gets().chomp()
+
+  if name.empty?()
+    prompt("Use a valid name.")
+  else
+    break
+  end
+end
+
+prompt("Hi #{name}")
+
+loop do # main loop
+  number1 = ''
+
+  loop do
+    prompt("What is the first number?")
+    number1 = Kernel.gets().chomp().to_i()
+    if valid_number?(number1)
+      break
+    else
+      prompt("Invalid input. Enter a number.")
+    end
+  end
+
+  number2 = ''
+  loop do
+    prompt("What is the second number?")
+    number2 = Kernel.gets().chomp().to_i()
+    if valid_number?(number2)
+      break
+    else
+      prompt("Invalid input. Enter a number.")
+    end
+  end
+
+  operator_prompt = <<-MSG
+  What operation would you like to perform?
+  1) Add
+  2) Subtract
+  3) Divide
+  4) Multiply
+  MSG
+
+  prompt(operator_prompt)
+
+  operator = ''
+  loop do
+    operator = Kernel.gets().chomp()
+
+    if %w(1 2 3 4).include?(operator)
+      break
+    else
+      prompt("Must choose 1, 2, 3, 4")
+    end
+  end
+
+  case operator
+  when "1"
+    result = number1 + number2
+  when "2"
+    result = number1 - number2
+  when "3"
+    result = number1.to_f() / number2.to_f()
+  when "4"
+    result = number1 * number2
+  end
+
+  prompt("The result is #{result}")
+
+  prompt("Would you like to do another operation? (y or n)")
+  answer = Kernel.gets().chomp()
+
+  break unless answer.downcase().start_with?('y')
+end
+
+prompt("Thank you for using the calculator, #{name}")
---
+++ mortgage_calc.rb
@@ -0,0 +1,24 @@
+=begin
+    Get inputs asking for:
+    - loan amount
+    - APR
+    - loan duration
+    Calculate:
+    - Monthly interest rate
+    - loan duration in months
+    - monthly payment
+
+    formula - m = P * (j / ( 1 - (1 + J)**(-n)))
+    m = monthly payment
+    p = loan amount
+    j = monthly interest rate
+    n = loan duration in months
+
+    after completing program:
+    run rubocop
+
+    Things to note:
+    What format should the inputs be in
+    If APR, convert to monthly
+    Clearly define the duration
+=end
