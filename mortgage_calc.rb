f2635105cf907550e5692be965e6b1033f4c200f
Aaron Potts <AaronFPotts@gmail.com>
2023-11-27 05:48:31 -0600

    Correct formula and start basic program

--- mortgage_calc.rb
+++ mortgage_calc.rb
@@ -3,12 +3,15 @@
     - loan amount
     - APR
     - loan duration
+
     Calculate:
     - Monthly interest rate
     - loan duration in months
     - monthly payment
 
     formula - m = P * (j / ( 1 - (1 + J)**(-n)))
+    Monthly payment = (loan amount) × (interest rate / 12) / (1 − (1 + (interest rate / 12)) ^ (-loan term))
+
     m = monthly payment
     p = loan amount
     j = monthly interest rate
@@ -22,3 +25,14 @@
     If APR, convert to monthly
     Clearly define the duration
 =end
+
+puts 'What is the loan amount?'
+loan_amount = gets.chomp.to_f
+puts "Length of loan?"
+duration = gets.chomp.to_f
+puts "Interest?"
+interest = (gets.chomp.to_f/100)
+# When relevant, it is expressed monthly, not yearly
+monthly_payment = loan_amount * ((interest/12) / (1 - (1 + (interest/12))**(-duration)))
+
+puts monthly_payment
